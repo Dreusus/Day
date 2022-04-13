5d4384760555db305d34b6207e0e9f11e3e79965
Andrey <dreususJ@yandex.ru>
2022-04-13 20:49:47 +0300

    feature: ez

--- js.js
+++ js.js
@@ -2,31 +2,35 @@ const todos = [
   'Сделать проектную работу',
   'Пройти туториал по Реакту',
   'Разобраться в замыканиях',
-]
+];
 
-const todosContainer = document.querySelector('.todos__list')
-
-const addTodoForm = document.querySelector('.todo-form')
-
-const input = document.querySelector('.todo-form__input')
+const todosContainer = document.querySelector('.todos__list');
+const addTodoForm = document.querySelector('.todo-form');
+const input = addTodoForm.querySelector('.todo-form__input')
 
 
 const createTodo = (taskName) => {
- return ` <li class="todo-item">
- <span class="todo-item__text">${taskName}</span>
- <button class="todo-item__edit"></button>
- <button class="todo-item__copy"></button>
- <button class="todo-item__del"></button>
+  const string = `<li class="todo-item">
+  <span class="todo-item__text"></span>
+  <button class="todo-item__edit"></button>
+  <button class="todo-item__copy"></button>
+  <button class="todo-item__del"></button>
 </li>`
+
+const element = document.createElement('div')
+element.insertAdjacentHTML('afterbegin', string);
+element.querySelector('.todo-item__text').textContent = taskName;
+return element.firstElementChild
 }
 
 
 const renderTodo = (taskName) => {
-  todosContainer.insertAdjacentHTML('afterbegin', createTodo(taskName))
+  todosContainer.append(createTodo(taskName))
 }
  
-const addTodo = (event) => {
-  event.preventDefault();
+const addTodo = (evt) => {
+  evt.preventDefault();
+  console.log(evt)
   const taskName = input.value;
   renderTodo(taskName)
   input.value= ''
@@ -36,7 +40,7 @@ const elements = todos.map(function(taskName) {
 return createTodo(taskName)
 })
 
-todosContainer.insertAdjacentHTML('afterbegin', elements.join('') ) 
+todosContainer.append(...elements)  
 
 addTodoForm.addEventListener('submit', addTodo)
 
